37bf9fa5826ee0a8642837a41d0350cbe7b20634
Elton Standerski <eltonstan@yahoo.com.br>
2026-05-04 21:30:41 -0300

    curso 01: aula 02 - Trilha: Kotlin com Spring Boot

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,29 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+
+@Service
+class TopicoService {
+
+    fun listar(): List<Topico> {
+        val topico = Topico(id = 1,
+            titulo = "Duvida Kotlin",
+            mensagem = "Variaveis no Kotlin",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programafcao"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Ana da Silva",
+                email = "ana@email.com"
+            ),
+        )
+        return listOf(topico, topico, topico)
+    }
+
+}
