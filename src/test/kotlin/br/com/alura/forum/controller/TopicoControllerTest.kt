d6b459b06a8cad4e641508276603c330cf283068
Elton Standerski <eltonstan@yahoo.com.br>
2026-06-28 20:40:47 -0300

    curso 04: aula 04 - Trilha: Kotlin com Spring Boot

---
+++ src/test/kotlin/br/com/alura/forum/controller/TopicoControllerTest.kt
@@ -0,0 +1,71 @@
+package br.com.alura.forum.controller
+
+import br.com.alura.forum.config.JWTUtil
+import br.com.alura.forum.model.Role
+import br.com.alura.forum.model.UsuarioTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TopicoControllerTest {
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    @Autowired
+    private lateinit var jwtUtil: JWTUtil
+
+    private lateinit var mockMvc: MockMvc
+
+    private var jwt: String? = null
+
+    private var token: String? = null
+
+    companion object {
+        private const val RECURSO = "/topicos/"
+        private const val RECURSO_ID = RECURSO.plus("%s")
+    }
+
+    @BeforeEach
+    fun setup() {
+        token = generateToken()
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+            .apply<DefaultMockMvcBuilder?>(
+                SecurityMockMvcConfigurers
+                    .springSecurity()).build()
+    }
+
+    @Test
+    fun `deve retornar codigo 400 quando chamar topicos sem token`() {
+        mockMvc.get(RECURSO).andExpect { status { is4xxClientError() } }
+    }
+
+    @Test
+    fun `deve retornar codigo 200 quando chamar topicos com token`() {
+        mockMvc.get(RECURSO) {
+            headers { token?.let { this.setBearerAuth(it) } }
+        }.andExpect { status { is2xxSuccessful() } }
+    }
+
+    @Test
+    fun `deve retornar codigo 200 quando chamar topico por id com token`() {
+        mockMvc.get(RECURSO_ID.format( "1")) {
+            headers { token?.let { this.setBearerAuth(it) } }
+        }.andExpect { status { is2xxSuccessful() } }
+    }
+
+    private fun generateToken() : String? {
+        val authorities = mutableListOf(Role(1, "LEITURA_ESCRITA"))
+        val usuario = UsuarioTest.buildToToken()
+
+        return jwtUtil.generateToken(usuario.email, authorities)
+    }
+}
